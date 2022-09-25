756e513e0c8d7c98e9dc472a9c4af0dc7deb9d9e
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-25 15:16:56 +0900

    パッケージ実装

--- src/script/dump.ts
+++ src/script/dump.ts
@@ -1,6 +1,8 @@
-import { dumpJson, fromJson } from 'json-microscope/node'
+import { fromJson, toJson } from 'json-microscope'
 import * as htmlTagsJson from '@vscode/web-custom-data/data/browsers.html-data.json'
 import _ from 'lodash'
+import fs from 'fs-extra'
+import path from 'path'
 
 type Attrs = {
   [tag in keyof HTMLElementTagNameMap]: string[]
@@ -12,4 +14,7 @@ export const attrs = _.mapValues(_.keyBy(htmlTags, 'name'), (val) =>
   val.attributes.map((attr: { name: keyof HTMLElementTagNameMap }) => attr.name)
 ) as Attrs
 
-dumpJson('./data/html-attrs.json', attrs)
+await fs.outputFile(
+  path.join(__dirname, '../data/html-attrs.json'),
+  toJson(attrs)
+)
